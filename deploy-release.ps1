9214656d669efd2f5396a98054be1cc66bc9f083
Tony Rasa <trasa@meancat.com>
2025-09-02 12:44:41 -0700

    fix branch for deploy

--- deploy-release.ps1
+++ deploy-release.ps1
@@ -1,11 +1,10 @@
 $ErrorActionPreference = "Stop"
 $outDir = "dist"
-$version = nbgv get-version -f json  | jq -r .NuGetPackageVersion
+$version = nbgv get-version -v NuGetPackageVersion
 
 # Upload to GitHub
 $tag = "v$version"
 gh release create $tag `
     --title "Release $tag" `
     --notes "Cross-platform build of FakeHttpClient" `
-    --target main `
     $(Get-ChildItem "$outDir\*.zip" | ForEach-Object { $_.FullName })
